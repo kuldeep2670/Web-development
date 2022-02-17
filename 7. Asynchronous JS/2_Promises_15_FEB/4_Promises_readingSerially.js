1b753009b1c65f4d22c3ec581f646e3ea34f813f
kuldeep2670 <kuldeepsingh2670@gmail.com>
2022-02-17 21:36:12 +0530

    promises

---
+++ 7. Asynchronous JS/2_Promises_15_FEB/4_Promises_readingSerially.js
@@ -0,0 +1,28 @@
+let fs = require('fs')
+
+console.log('before')
+
+let f1p = fs.promises.readFile('f1.txt')
+
+f1p.then(cb1)
+
+function cb1(data){
+    console.log('File data-->' + data)
+    
+    let f2p = fs.promises.readFile('f2.txt')
+    f2p.then(cb2)
+}
+
+function cb2(data){
+    console.log('File data-->' + data)
+    
+    let f3p = fs.promises.readFile('f3.txt')
+    f3p.then(cb3)
+}
+
+function cb3(data){
+    console.log('File data-->' + data)
+}
+
+
+console.log('after')
