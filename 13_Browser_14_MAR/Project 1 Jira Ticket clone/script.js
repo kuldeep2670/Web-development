f16b6ced789c4a9badee7acbd9cf36e1cc839c4b
kuldeep2670 <kuldeepsingh2670@gmail.com>
2022-03-27 22:41:02 +0530

    designing done

--- 13_Browser_14_MAR/Project 1 Jira Ticket clone/script.js
+++ 13_Browser_14_MAR/Project 1 Jira Ticket clone/script.js
@@ -1,8 +1,18 @@
 let addBtn = document.querySelector(".add-btn")
 
+let modalCont = document.querySelector(".modal-cont")
+
+let addFlag = false
 addBtn.addEventListener("click", function(e){
-    //display a modal
+    //display the modal
+    //addFalg, true - modal display
+    //addFlag, false = modal hide
 
+    addFlag = !addFlag
+    if(addFlag == true){
+        modalCont.style.display = 'flex'
+    } else{
+        modalCont.style.display= 'none'
+    }
 
-    // add a card
-})
+});
