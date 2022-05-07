5a7184e32602b09c973881f2e574073507671e7b
kuldeep2670 <kuldeepsingh2670@gmail.com>
2022-05-07 13:37:10 +0530

    movie-button

--- 15_REACT/movieapp/src/components/MoviesList.js
+++ 15_REACT/movieapp/src/components/MoviesList.js
@@ -15,8 +15,13 @@ export class MoviesList extends Component {
           {
             moviesArr.map((movieElem) => (
               <div className="card movie-card">
-                <img src="https://images7.alphacoders.com/117/1174976.jpg" style={{height:'40vh', width: '20vw'}}className="card-img-top movie-img" alt="..." />
-                <h5 className="card-title banner-title">{movieElem.title}</h5>
+                <img src="https://images7.alphacoders.com/117/1174976.jpg" style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
+                <h5 className="card-title movie-title">{movieElem.title}</h5>
+
+                <div className="button-wrapper" style={{display:'flex', justifyContent:'center'}}>
+                  <a href="#" className="btn btn-primary movies-button text-center">Add to Favourites</a>
+                </div>
+
               </div>
             ))
           }
