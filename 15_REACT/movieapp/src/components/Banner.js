5fab8d52bec5c2a16d24ab76203e653f79428ed5
kuldeep2670 <kuldeepsingh2670@gmail.com>
2022-05-08 23:36:24 +0530

    pagination

--- 15_REACT/movieapp/src/components/Banner.js
+++ 15_REACT/movieapp/src/components/Banner.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
+//import {movies} from '../movieData'
 
 export class Banner extends Component {
     render() {
+        // let moviesArr = movies.results[Math.floor((Math.random() * 10) + 1)];
+        // let backDrop = moviesArr.backdrop_path
+        let backDrop = '/6EdKBYkB1ssgGjc249ud1L55o8d.jpg'
+
         return (
             <div className="card banner-card">
-                <img src="https://images7.alphacoders.com/117/1174976.jpg" className="card-img-top banner-img" alt="..." />
-                <h5 className="card-title banner-title">Iron Man</h5>
-                <p className="card-text banner-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <img src={`https://image.tmdb.org/t/p/original${backDrop}`} className="card-img-top banner-img" alt="..." />
+                <h5 className="card-title banner-title">Jurassic Hunt</h5>
+                <p className="card-text banner-text">Female adventurer Parker joins a crew of male trophy hunters in a remote wilderness park. Their goal: slaughter genetically recreated dinosaurs for sport using rifles, arrows, and grenades. After their guide is killed by raptors, the team tries to escape the park – but the hunters quickly become the hunted. Even worse, the park’s manager suspects Parker of being a spy and sends a hit squad after her. This battle’s about to become primitive!</p>
             </div>
         )
     }
--- 15_REACT/movieapp/src/components/MoviesList.js
+++ 15_REACT/movieapp/src/components/MoviesList.js
@@ -1,10 +1,68 @@
 import React, { Component } from "react";
-import { movies } from "../movieData";
+//import { movies } from "../movieData";
+
+import axios from 'axios'
 
 export class MoviesList extends Component {
-  render() {
-    let moviesArr = movies.results
 
+  constructor() { 
+    super()
+    this.state = {
+      hover: '',
+      parr: [1],
+      movies: [],
+      currPage: 1,
+    }
+  }
+
+  async componentDidMount() {
+      const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=e091c04a3856b50c36595519d4abbdd2&language=en-US&page=${this.state.currPage}`)
+      let movieData = res.data
+      //console.log(movieData)
+      this.setState({
+        movies: [...movieData.results]
+      })
+  }
+
+  changeMovies = async() =>{
+    const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=e091c04a3856b50c36595519d4abbdd2&language=en-US&page=${this.state.currPage}`)
+      let movieData = res.data
+      //console.log(movieData)
+      this.setState({
+        movies: [...movieData.results]
+      })
+  }
+
+  handleNext = () =>{
+    let tempArr = []
+    for(let i = 1;  i <= this.state.parr.length + 1; i++ ){
+      tempArr.push(i)
+    }
+    this.setState({
+      parr: [...tempArr],
+      currPage: this.state.currPage + 1,
+    }, this.changeMovies)
+    
+  }
+
+  handlePrevious = () =>{
+    if(this.state.currPage != 1){
+      this.setState({
+        currPage: this.state.currPage - 1
+      }, this.changeMovies)
+    }
+  }
+
+  handlePageClick = (value) =>{
+    if(value != this.state.currPage){
+      this.setState({
+        currPage: value
+      }, this.changeMovies)
+    }
+  }
+
+  render() {
+    
     return (
       <>
         <div>
@@ -13,18 +71,34 @@ export class MoviesList extends Component {
 
         <div className="movies-list">
           {
-            moviesArr.map((movieElem) => (
-              <div className="card movie-card">
-                <img src="https://images7.alphacoders.com/117/1174976.jpg" style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
-                <h5 className="card-title movie-title">{movieElem.title}</h5>
+            this.state.movies.map((movieElem) => (
+              <div className="card movie-card" onMouseEnter={() => this.setState({ hover: movieElem.id })} onMouseLeave={() => this.setState({ hover: '' })}>
+                <img src={`https://image.tmdb.org/t/p/original${movieElem.backdrop_path}`} style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
+                <h5 className="card-title movie-title">{movieElem.original_title}</h5>
 
-                <div className="button-wrapper" style={{display:'flex', justifyContent:'center'}}>
-                  <a href="#" className="btn btn-primary movies-button text-center">Add to Favourites</a>
+                <div className="button-wrapper" style={{ display: 'flex', justifyContent: 'center' }}>
+                  {
+                    this.state.hover == movieElem.id && <a href="#" className="btn btn-primary movies-button text-center">Add to Favourites</a>
+                  }
                 </div>
 
               </div>
             ))
           }
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <nav aria-label="Page navigation example">
+            <ul className="pagination">
+              <li className="page-item"><a className="page-link" onClick={this.handlePrevious}>Previous</a></li>
+              {
+                this.state.parr.map((value) => (
+                  <li className="page-item"><a className="page-link" onClick={() => this.handlePageClick(value)}>{value}</a></li>
+                ))
+              }
+
+              <li className="page-item"><a className="page-link" onClick={this.handleNext}>Next</a></li>
+            </ul>
+          </nav>
         </div>
       </>
     );
