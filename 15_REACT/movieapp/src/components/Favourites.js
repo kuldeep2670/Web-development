062d5b6f1ef871b43ca549da23ed90235f08e1a1
kuldeep2670 <kuldeepsingh2670@gmail.com>
2022-05-15 20:37:22 +0530

    favourites table

--- 15_REACT/movieapp/src/components/Favourites.js
+++ 15_REACT/movieapp/src/components/Favourites.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react'
 
+import { movies } from '../movieData'
+
 export class Favourites extends Component {
   render() {
+    const moviesArr = movies.results;
+    //console.log(moviesArr);
+    let genreids = {
+      28: "Action",
+      12: "Adventure",
+      16: "Animation",
+      35: "Comedy",
+      80: "Crime",
+      99: "Documentary",
+      18: "Drama",
+      10751: "Family",
+      14: "Fantasy",
+      36: "History",
+      27: "Horror",
+      10402: "Music",
+      9648: "Mystery",
+      10749: "Romance",
+      878: "Sci-Fi",
+      10770: "TV",
+      53: "Thriller",
+      10752: "War",
+      37: "Western",
+    };
     return (
       <>
         <div className='main'>
@@ -18,49 +43,51 @@ export class Favourites extends Component {
 
             <div className='col-9 favourites-table'>
               <div className='row'>
-                <input type='text' className='input-group-text col' />
+                <input placeholder='Search' type='text' className='input-group-text col' />
                 <input type='number' className='input-group-text col' />
               </div>
 
               <div className='row'>
-                <table class="table">
+                <table className="table">
                   <thead>
                     <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">First</th>
-                      <th scope="col">Last</th>
-                      <th scope="col">Handle</th>
+
+                      <th scope="col"></th>
+                      <th scope="col">Title</th>
+                      <th scope="col">Genre</th>
+                      <th scope="col">Popularity</th>
+                      <th scope="col">Rating</th>
+                      <th scope="col"></th>
+                      
+                      
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td>Mark</td>
-                      <td>Otto</td>
-                      <td>@mdo</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">2</th>
-                      <td>Jacob</td>
-                      <td>Thornton</td>
-                      <td>@fat</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">3</th>
-                      <td colspan="2">Larry the Bird</td>
-                      <td>@twitter</td>
-                    </tr>
+                    {
+                      moviesArr.map((movieElem) => (
+                        <tr>
+                          <td><img style ={{width:'6rem'}} src ={`https://image.tmdb.org/t/p/original${movieElem.backdrop_path}`} /></td>
+                          <th scope="row">{movieElem.title}</th>
+                          <td>{genreids[movieElem.genre_ids[0]]}</td>
+                          <td>{movieElem.popularity}</td>
+                          <td>{movieElem.vote_average}</td>
+                          <td><button type="button" class="btn btn-danger">Delete</button></td>
+                        </tr>
+                      ))
+                    }
+
+
                   </tbody>
                 </table>
               </div>
 
               <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                  <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                  <li class="page-item"><a class="page-link" href="#">1</a></li>
-                  <li class="page-item"><a class="page-link" href="#">2</a></li>
-                  <li class="page-item"><a class="page-link" href="#">3</a></li>
-                  <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                <ul className="pagination">
+                  <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                  <li className="page-item"><a className="page-link" href="#">1</a></li>
+                  <li className="page-item"><a className="page-link" href="#">2</a></li>
+                  <li className="page-item"><a className="page-link" href="#">3</a></li>
+                  <li className="page-item"><a className="page-link" href="#">Next</a></li>
                 </ul>
               </nav>
             </div>
