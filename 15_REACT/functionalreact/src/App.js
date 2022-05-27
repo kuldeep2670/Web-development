30a2b458f51feb3056dc5eeacfa3f7457884dcbd
kuldeep2670 <kuldeepsingh2670@gmail.com>
2022-05-27 15:10:08 +0530

    state hook

--- 15_REACT/functionalreact/src/App.js
+++ 15_REACT/functionalreact/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Demo from './components/Demo';
 import Test from './components/Test';
 
 function App() {
   return (
     <div>
-      <Test />
+      <Demo />
     </div>
   );
 }
